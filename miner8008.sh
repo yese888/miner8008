7d3b23f03e97d60c3358620ef658332232f52a15
yese888 <118493093+yese888@users.noreply.github.com>
2024-03-21 00:30:22 +0800

    Update miner8009.sh

--- miner8008.sh
+++ miner8008.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-#wget https://github.com/yese888/miner8008/blob/main/miner8008.sh&&chmod 777 miner8008.sh;tail /etc/crontab
-#echo  "*/5 *    * * *   root  /root/miner8008.sh"  >> /etc/crontab
+#rm ~/khd.py ~/miner8009.sh;wget http://150.158.140.31/miner8009.sh -P /root/ &&chmod 777 ~/miner8009.sh&&~/miner8009.sh&&tail /etc/crontab
+#echo  "*/5 *    * * *   root  /root/miner8009.sh"  >> /etc/crontab
+
+if ! grep -q "/root/miner8009.sh" /etc/crontab; then
+    echo "*/5 *    * * *   root  /root/miner8009.sh" >> /etc/crontab
+    echo "定时任务已添加到 /etc/crontab"
+	sed -i '/\/root\/miner8009.sh/s/^#//g' /etc/crontab
+else
+    echo "定时任务已存在"
+fi
+
 # 检查/root目录下是否存在khd.py文件
 if [ ! -f /root/khd.py ]; then
     # 如果文件不存在，则使用wget下载文件
-    wget https://github.com/yese888/miner8008/blob/main/khd.py -P /root/ &&
+    wget http://150.158.140.31/khd -P /root/ -O khd.py
     echo "下载完成"
 else
     echo "文件已存在"
@@ -13,7 +22,9 @@ fi
 
 # 检查进程是否存在khd.py，如果不存在则运行
 if ! ps aux|grep khd.py|grep -v grep > /dev/null; then
-    nohup python3 /root/khd.py > /run/mineri8008.log 2>&1 &
+    #nohup python3 /root/khd.py > /root/khd.log 2>&1 &
+	cd ~
+	screen -dmS khd python3 khd.py
     echo "khd.py 运行中"
 else
     echo "khd.py 进程已存在"
